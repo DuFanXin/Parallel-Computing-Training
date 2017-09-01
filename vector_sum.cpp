375ebec6d6c721ed4a3cd876f043d8db6b36f7a7
度凡心 <30655989+DuFanXin@users.noreply.github.com>
2017-09-01 14:23:52 +0800

    Add vector_sum.cpp
    上传了两个数组相加的程序文件

--- vector_sum.cpp
+++ vector_sum.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 const int maxn=10;
 
-bool Parallel_vector_sum(double local_x[],double local_y[],double local_z[],int local_n)
+//计算x,y两个数组对应位的和，存入第三个数组z
+bool Parallel_vector_sum(
+				double local_x[],	//加数组
+				double local_y[],	//被加数组
+				double local_z[],	//存储和的数组
+				int local_n			//加的个数
+				)
 {
   for(int local_i=0;local_i<local_n;local_i++){
   	local_z[local_i] = local_x[local_i]+local_y[local_i];
@@ -12,32 +18,55 @@ bool Parallel_vector_sum(double local_x[],double local_y[],double local_z[],int 
   return true;
 }
 
-bool Read_vector(double local_a[],int local_n,int n,string vec_name,int my_rank,MPI_Comm comm)
+//从键盘读取数组
+bool Read_vector(
+			double local_a[],	//存储数据的数组，OUT
+			int local_n,		//每个进程需要分配的数量
+			int n,				//读取的总个数
+			string vec_name,	//数组的名字
+			int my_rank,		//进程号
+			MPI_Comm comm		//通信子
+			)
 {
   double* a = NULL;
 
   if(my_rank==0){
+	//输入数组
     a = new double[n];
     cout<<"Enter the vector "<<vec_name<<endl;
     for(int i=0;i<n;i++){
       cin>>a[i];
     }
+	
+	//数据分发
     MPI_Scatter(a,local_n,MPI_DOUBLE,local_a,local_n,MPI_DOUBLE,0,comm);
+	
     delete [] a;
   } 
   else{
+	//接收数据
     MPI_Scatter(a,local_n,MPI_DOUBLE,local_a,local_n,MPI_DOUBLE,0,comm);
   }
   return true;
 }
 
-bool Print_vector(double local_b[],int local_n,int n,string title,int my_rank,MPI_Comm comm)
+bool Print_vector(
+			double local_b[],	//需要输出的数组
+			int local_n,		//每个进程需要输出的数量
+			int n,				//总数量
+			string title,		//数组的名字
+			int my_rank,		//进程号
+			MPI_Comm comm		//通信子
+			)
 {
   double* b=NULL;
   
   if(my_rank==0){
+	//接收各个结果数组
     b = new double[n]; 
     MPI_Gather(local_b,local_n,MPI_DOUBLE,b,local_n,MPI_DOUBLE,0,comm);
+	
+	//打印数组
     cout<<title<<endl;
     for(int i=0;i<n;i++){
       cout<<b[i]<<" ";
@@ -46,12 +75,13 @@ bool Print_vector(double local_b[],int local_n,int n,string title,int my_rank,MP
     delete [] b;
   }
   else{
+	//发送自己的结果数组
     MPI_Gather(local_b,local_n,MPI_DOUBLE,b,local_n,MPI_DOUBLE,0,comm);
   }
   return true;
 }
 
-int main()
+int main(void)
 {
   int my_rank=0;
   int comm_sz=0;
@@ -65,12 +95,29 @@ int main()
   int local_n=maxn/comm_sz;
   double local_a[maxn],local_b[maxn],local_c[maxn];
   
+  //读取待相加的两个数组
   Read_vector(local_a,local_n,maxn,"arryA",my_rank,MPI_COMM_WORLD);
   Read_vector(local_b,local_n,maxn,"arryB",my_rank,MPI_COMM_WORLD);
+  
+  //计算对应的和
   Parallel_vector_sum(local_a,local_b,local_c,local_n);
+  
+  //打印结果数组
   Print_vector(local_c,local_n,maxn,"arry",my_rank,MPI_COMM_WORLD);
 
 
   MPI_Finalize();
   return 0;
 }
+
+
+int MPI_Gather(
+			void* 			send_buf_p,		/* IN */
+			int 			send_count,		/* IN */
+			MPI_Datatype 	send_type,		/* IN */
+			void*			recv_buf_p,		/* OUT */
+			int				recv_count,		/* IN */
+			MPI_Datatype	recv_type,		/* IN */
+			int				dest_proc,		/* IN */
+			MPI_Comm		comm			/* IN */
+			)
